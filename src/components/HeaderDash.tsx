955429905d694941f112fa4abf8abcf7b2304ced
Jonatas <jonatasserra@outlook.com>
2022-10-16 19:24:24 -0300

    styles(HeaderDash): Notification

--- src/components/HeaderDash.tsx
+++ src/components/HeaderDash.tsx
@@ -65,7 +65,6 @@ const HeaderDash: React.FC<HeaderDashProps> = ({ handleMenu, isHidden }) => {
             isHidden ? 'hidden' : ''
           } inline-flex items-center justify-center p-2 text-sm text-zinc-50 rounded-lg sm:hidden hover:bg-orange-500`}
         >
-          <span className="sr-only">Open main menu</span>
           <svg
             className="w-6 h-6"
             aria-hidden="true"
@@ -80,59 +79,82 @@ const HeaderDash: React.FC<HeaderDashProps> = ({ handleMenu, isHidden }) => {
             ></path>
           </svg>
         </button>
-        <div className={`flex-col justify-center items-center`}>
-          <div className="flex">
-            <button
-              onClick={() => setIsOpen(!IsOpen)}
-              className="flex items-center text-sm font-medium text-zinc-50 rounded-full hover:text-orange-500 md:mr-0 focus:ring-2 focus:ring-gray-100 dark:focus:ring-orange-500"
-            >
-              <img
-                className="mr-2 w-12 h-12 rounded-full"
-                src={user?.image}
-                alt="user photo"
-              ></img>
-              {user?.name}
+        <div className="flex items-center justify-center">
+          <div className="mr-4 mt-2">
+            <button>
               <svg
-                className="w-4 h-4 mx-1.5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
+                className="w-8 h-8"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fillRule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clipRule="evenodd"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                 ></path>
               </svg>
+              <div className="inline-flex justify-center absolute top-4 items-center z-20 w-5 h-5 text-xs font-bold text-white bg-red-500 rounded-full">
+                2
+              </div>
             </button>
           </div>
-          <div className="relative">
-            <div
-              className={`flex-col ${
-                IsOpen ? '' : 'hidden'
-              }  z-10 w-full divide-y rounded-xl mt-2 shadow bg-zinc-900 divide-gray-800 absolute  origin-top-right bg-white ring-1 ring-orange-500 focus:outline-none" `}
-              role="menu"
-              aria-orientation="vertical"
-              aria-labelledby="menu-button"
-            >
-              <ul className="w-full h-full text-sm text-zinc-50">
-                <li className="hover:bg-zinc-700 rounded-xl">
-                  <Link
-                    to={'settings'}
-                    onClick={() => setIsOpen(false)}
-                    className="text-center w-full block py-2 px-4  hover:text-orange-500"
-                  >
-                    Configurações
-                  </Link>
-                </li>
-              </ul>
-              <div className="py-1 rounded-xl outline-2 hover:bg-zinc-700 ">
-                <button
-                  onClick={signOut}
-                  className="w-full block py-2 px-4 text-sm text-zinc-50  hover:text-orange-500"
+          <div className={`flex-col justify-center items-center`}>
+            <div className="flex">
+              <button
+                onClick={() => setIsOpen(!IsOpen)}
+                className="flex items-center text-sm font-medium text-zinc-50 rounded-full hover:text-orange-500 md:mr-0 focus:ring-2 focus:ring-gray-100 dark:focus:ring-orange-500"
+              >
+                <img
+                  className="mr-2 w-12 h-12 rounded-full"
+                  src={user?.image}
+                  alt="user photo"
+                ></img>
+                {user?.name}
+                <svg
+                  className="w-4 h-4 mx-1.5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  Sair
-                </button>
+                  <path
+                    fillRule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+            <div className="relative">
+              <div
+                className={`flex-col ${
+                  IsOpen ? '' : 'hidden'
+                }  z-10 w-full divide-y rounded-xl mt-2 shadow bg-zinc-900 divide-gray-800 absolute  origin-top-right bg-white ring-1 ring-orange-500 focus:outline-none" `}
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="menu-button"
+              >
+                <ul className="w-full h-full text-sm text-zinc-50">
+                  <li className="hover:bg-zinc-700 rounded-xl">
+                    <Link
+                      to={'settings'}
+                      onClick={() => setIsOpen(false)}
+                      className="text-center w-full block py-2 px-4  hover:text-orange-500"
+                    >
+                      Configurações
+                    </Link>
+                  </li>
+                </ul>
+                <div className="py-1 rounded-xl outline-2 hover:bg-zinc-700 ">
+                  <button
+                    onClick={signOut}
+                    className="w-full block py-2 px-4 text-sm text-zinc-50  hover:text-orange-500"
+                  >
+                    Sair
+                  </button>
+                </div>
               </div>
             </div>
           </div>
