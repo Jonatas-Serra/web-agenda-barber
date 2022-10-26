1b2803207c2df6f4fa9e4b83407b2f9f18cb4d9b
Jonatas <jonatasserra@outlook.com>
2022-10-25 22:02:00 -0300

    feat(Auth): verify if token exp. and signOut user

--- src/pages/Dash.tsx
+++ src/pages/Dash.tsx
@@ -1,11 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import HeaderDash from '../components/HeaderDash'
 import NavSide from '../components/NavSide'
+import api from '../services/api'
+import { useAuth } from '../hooks/Auth'
+import { useToast } from '../hooks/Toast'
 
 const Dash: React.FC = () => {
   const [isOpenMenu, setIsOpenMenu] = useState(false)
   const [isHidden, setIsHidden] = useState(false)
+  const { signOut } = useAuth()
+  const { addToast } = useToast()
+
+  const verifyToken = () => {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      api.post('auth/check', { token }).then((response) => {
+        if (!response.data.success) {
+          addToast({
+            type: 'error',
+            title: 'Sessão expirada',
+            description: 'Faça login novamente',
+          })
+          setTimeout(() => {
+            signOut()
+          }, 2000)
+        }
+      })
+    }
+  }
 
   const handleMenu = () => {
     setIsOpenMenu(!isOpenMenu)
@@ -16,6 +40,11 @@ const Dash: React.FC = () => {
     setIsOpenMenu(false)
     setIsHidden(false)
   }
+
+  useEffect(() => {
+    verifyToken()
+  }, [])
+
   return (
     <main className="h-screen flex bg-zinc-300">
       <div className={`${isOpenMenu ? 'flex' : 'hidden'} sm:flex`}>
