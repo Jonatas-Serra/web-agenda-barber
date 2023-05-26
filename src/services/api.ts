2520498aeb1d49aded192e47048f7042650ab4f8
Jonatas <jonatasserra@outlook.com>
2023-05-26 01:03:20 -0300

    conection api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://apiagendabarber.brazilsouth.cloudapp.azure.com/',
-  // baseURL: 'http://localhost:3000/',
+  // baseURL: 'https://apiagendabarber.brazilsouth.cloudapp.azure.com/',
+  baseURL: 'http://146.190.177.83/',
 })
 
 export default api
